7cfa91001b619e5452b5cbb5e00d4411b8ca2a02
Hi-Fi <juho.saarinen@gmail.com>
2019-05-22 19:41:13 +0300

    3.2: puhelinluettelon backend step2

--- index.js
+++ index.js
@@ -24,11 +24,15 @@ let persons = [
     }   
 ]
   
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
-  
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.get('/info', (req, res) => {
+    res.status(200).send(`<p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p><p>${Date()}</p>`)
+})
+
+app.get('/api/persons', (req, res) => {
+res.json(persons)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)
+})
