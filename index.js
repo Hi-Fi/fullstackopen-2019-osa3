d8e7398a85303f9c249413c70f950f4989f0b88c
Hi-Fi <juho.saarinen@gmail.com>
2019-05-22 19:25:21 +0300

    3.1 puhelinluettelon backend step1

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+
+let persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "045-1236543",
+      "id": 1
+    },
+    {
+        "name": "Arto Järvinen",
+        "number": "041-21423123",
+        "id": 2
+    },
+    {
+        "name": "Lea Kutvonen",
+        "number": "040-4323234",
+        "id": 3
+    },
+    {
+      "name": "Martti Tienari",
+      "number": "09-784232",
+      "id": 4
+    }   
+]
+  
+  app.get('/api/persons', (req, res) => {
+    res.json(persons)
+  })
+  
+  const PORT = 3001
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
