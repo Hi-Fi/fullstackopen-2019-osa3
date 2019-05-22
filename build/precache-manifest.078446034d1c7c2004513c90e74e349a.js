5aaf988e2d913176d6477ef8cef9d531bc354091
Hi-Fi <juho.saarinen@gmail.com>
2019-05-22 20:56:40 +0300

    uibuild

---
+++ build/precache-manifest.078446034d1c7c2004513c90e74e349a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bd702cc54423f6f81f55b184f482819",
+    "url": "/index.html"
+  },
+  {
+    "revision": "44e39b1bdc670cee02b9",
+    "url": "/static/css/main.08afd48b.chunk.css"
+  },
+  {
+    "revision": "a8a3fb159b8ee07a1f4d",
+    "url": "/static/js/2.5eca492c.chunk.js"
+  },
+  {
+    "revision": "44e39b1bdc670cee02b9",
+    "url": "/static/js/main.888e2c25.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ morgan.token('body', function getBody (req) {
     return JSON.stringify(req.body)
   })
 
+app.use(express.static('build'))
 app.use(bodyParser.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
